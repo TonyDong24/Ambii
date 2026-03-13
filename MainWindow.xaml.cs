dfaa6e9ace6a47b6f2401ed0844deaaf339f7dae
TonyDong24 <duy2482002@gmail.com>
2026-03-13 16:44:29 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Ambii.Views; // Đảm bảo bạn đã tạo thư mục Views
+
+namespace Ambii
+{
+    public partial class MainWindow : Window
+    {
+        // Tạo một biến static để các màn hình con có thể gọi MainWindow dễ dàng
+        public static MainWindow Instance { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            this.Navigate(new StartView());
+
+            // Tạm thời để trống, sau khi tạo xong StartView mình sẽ quay lại đây
+        }
+
+        // Hàm dùng để đổi màn hình
+        public void Navigate(UserControl nextView)
+        {
+            MainContentHolder.Content = nextView;
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // Kiểm tra nếu phím vừa nhấn là ESC
+            if (e.Key == System.Windows.Input.Key.Escape)
+            {
+                // Đóng toàn bộ ứng dụng ngay lập tức
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+    }
+}
---
+++ Views/StartView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ambii.Views
+{
+    public partial class StartView : UserControl
+    {
+        public StartView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // Khi ấn nút này, nó sẽ thông báo cho MainWindow chuyển sang trang tiếp theo
+            // Tạm thời chưa có trang tiếp theo nên ta sẽ để đây.
+            MessageBox.Show("Đã nhấn Start! Sẽ chuyển sang chọn Frame.");
+        }
+        private void BtnAdminSetting_Click(object sender, RoutedEventArgs e)
+        {
+            // Tạm thời hiển thị thông báo để test
+            MessageBox.Show("Chào Admin! Chức năng cấu hình hệ thống đang được phát triển.");
+        }
+    }
+}
