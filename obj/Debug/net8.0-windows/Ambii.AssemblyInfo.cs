4f33714333df097a3778017de2113e10104027d3
TonyDong24 <duy2482002@gmail.com>
2026-03-24 15:29:09 +0700

    frame_selectionv2

--- obj/Debug/net8.0-windows/Ambii.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Ambii.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ambii")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6675a0b824bacb523e8f1f45bfaeb4ce653925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaef0d5c369390aa99dba86a44fe62a3bf3873b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ambii")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ambii")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
